4304c0d274a4fef90c1d584ff21b7a80d4f33500
jwilson42 <wilson.jd022@gmail.com>
2021-02-15 23:40:20 -0800

    added more features to sign up page, can now preview images before submitting

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,3 +2,9 @@ document.querySelector('#auth-privacy').addEventListener('change', e => {
     document.querySelector('#publicInfo').classList.toggle('hidden');
     document.querySelector('#privateInfo').classList.toggle('hidden');
 });
+
+document.querySelector('#auth-image').addEventListener('change', e => {
+    if (e.target.files.length > 0) {
+        document.querySelector('#imagePreview').src = URL.createObjectURL(e.target.files[0]);
+    }
+});
